eef5d272db3c313fbc394b32e4ba3cdb42951e68
Khadijah Kasiran <khadijah.lkc@gmail.com>
2026-03-15 08:50:51 +0800

    Fix database table creation for gunicorn

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ from app.models import User, LoanApplication, Prediction
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'LoanApplication': LoanApplication, 'Prediction': Prediction}
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=5000)
